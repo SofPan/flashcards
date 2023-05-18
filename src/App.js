02899d7dc3ff90532726b955f7b311e672e531ff
SofPan <84824782+SofPan@users.noreply.github.com>
2023-05-18 12:36:02 -0400

    Set up cards table

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import React, {useState} from 'react'
+import CardTable from './tables/CardTable';
 
 function App() {
+  const sampleCards = [
+    {id: 1, question: "What is the capital of France?", answer: "Paris"},
+    {id: 2, question: "What is Canada's national sport?", answer: "Lacrosse"},
+    {id: 3, question: "Who is credited for inventing the first computer?", answer: "Charles Babbage"}
+  ]
+
+  const [cards, setCard] = useState(sampleCards);
+
   return (
     <div className="container">
-
+      <h1>Flashcards</h1>
+      <div className="cards">
+        {/* Cards render here */}
+        {/* Reset and shuffle buttons */}
+      </div>
+      <div className="options">
+        <h2>Card Options</h2>
+        <CardTable cards={cards}/>
+      </div>
     </div>
   );
 }
---
+++ src/tables/CardTable.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function CardTable(props){
+    return(
+        <table>
+            <thead>
+                <tr>
+                    <th>Question</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                {props.cards.length > 0 ? (
+                    props.cards.map((card) => (
+                        <tr key={card.id}>
+                            <td>{card.question}</td>
+                            <button>Edit</button>
+                            <button>Delete</button>
+                        </tr>
+                    ))
+                ) : props.cards.length === 0 (
+                    <tr>
+                        <td colSpan={3}>No Cards</td>
+                    </tr>
+                )}
+            </tbody>
+        </table>
+    )
+}
+
+export default CardTable;
