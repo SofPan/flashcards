f40990249d47d0ca725680b53a2dd656cc60da1c
SofPan <84824782+SofPan@users.noreply.github.com>
2023-05-19 11:47:23 -0400

    setting up next card button

--- src/cards/Card.js
+++ src/cards/Card.js
@@ -4,6 +4,25 @@ const Card = (props) => {
     const [showAnswer, setShowAnswer] = useState(false);
     const [currentCard, setCurrentCard] = useState(props.showing);
 
+    const handlePrev = (event) => {
+        event.preventDefault();
+        const newId = currentCard.id - 1;
+        if (newId <= 1){
+            alert('there is no previous card');
+            setCurrentCard(currentCard);
+        }
+        setCurrentCard(props.cards.map((card) => (card.id === newId)));
+    }
+
+    const handleNext = (event) => {
+        event.preventDefault();
+        const newId = currentCard.id + 1;
+        if (newId >= props.cards.length - 1){
+            alert('there is no next card');
+            setCurrentCard(currentCard);
+        }
+        setCurrentCard(props.cards.map((card) => (card.id === newId)));
+    }
 
     return(
         <div className="flashcard">
@@ -18,8 +37,8 @@ const Card = (props) => {
                 )}
             </div>
             <div>
-                <button>Prev</button>
-                <button>Next</button>
+                <button onClick={handlePrev}>Prev</button>
+                <button onClick={handleNext}>Next</button>
             </div>
         </div>
     )
