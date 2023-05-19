bdf11513b5bb93217fda8882930e9bec1840efda
SofPan <84824782+SofPan@users.noreply.github.com>
2023-05-19 11:17:14 -0400

    Conditional to show question or answer

--- src/cards/Card.js
+++ src/cards/Card.js
@@ -1,12 +1,22 @@
 import React, {useState} from "react";
 
 const Card = (props) => {
-    // const [showAnswer, setShowAnswer] = useState(false);
-    const testReturn = props.cardArr[0];
+    const [showAnswer, setShowAnswer] = useState(false);
+    const [currentCard, setCurrentCard] = useState(props.cardArr[0]);
+
+
     return(
         <div className="flashcard">
-            <h2>{testReturn.question}</h2>
-            <p>{testReturn.answer}</p>
+            <button onClick={() => setShowAnswer(!showAnswer)}>
+                <span className="flip"></span>
+            </button>
+            <div>
+                {showAnswer ? (
+                    <p className="card-answer">{currentCard.answer}</p>
+                ) : (
+                    <h3>{currentCard.question}</h3>
+                )}
+            </div>
         </div>
     )
 }
